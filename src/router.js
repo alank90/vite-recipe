3f4abf2dc1864115401d888c80486f5534fab9bd
Alan K <akillian@outlook.com>
2021-09-14 13:57:20 -0400

    First commit

---
+++ src/router.js
@@ -0,0 +1,10 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", name: "index", component: () => import("@/views/index.vue") },
+    { path: "/", name: "index", component: () => import("@/views/index.vue") },
+
+  ],
+});
