90498219692bad6a84595eefdbbf4f4242107c28
Alan K <akillian@outlook.com>
2021-09-15 14:21:38 -0400

    commit

--- src/router.js
+++ src/router.js
@@ -3,8 +3,13 @@ import { createRouter, createWebHistory } from "vue-router";
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/", name: "index", component: () => import("@/views/index.vue") },
-    { path: "/", name: "index", component: () => import("@/views/index.vue") },
-
+    { path: "/", name: "index", component: () => import("./views/index.vue") },
+    {
+      path: "/recipes:id",
+      name: "single-recipe",
+      component: () => import("./views/_uid.vue"),
+    },
   ],
 });
+
+export default router;
