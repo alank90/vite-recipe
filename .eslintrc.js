5571d39cac1a513c248d53dcd7a1e82bc778e31b
Alan Killian <akillian@outlook.com>
2021-09-24 21:10:41 -0400

    commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
-        "plugin:vue/essential"
+        "plugin:vue/essential",
+        "prettier"
     ],
     "parserOptions": {
         "ecmaVersion": 12,
