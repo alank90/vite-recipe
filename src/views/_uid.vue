12d8d8d14f58a0cfb4c56184f5dfadbc793ab742
Alan K <akillian@outlook.com>
2021-09-29 14:20:25 -0400

    commit

--- src/views/_uid.vue
+++ src/views/_uid.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>{{data}}</div>
+  <div>Recipe Here</div>
 </template>
 
 <script setup>
@@ -7,5 +7,6 @@ import { useRoute } from "vue-router";
 import { usePrismicDocumentByUID } from "@prismicio/vue";
 
 const route = useRoute();
+// eslint-disable-next-line no-unused-vars
 const { data } = usePrismicDocumentByUID("recipes", route.params.uid);
 </script>
